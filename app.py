f81159a8a578ec64db8cba2e506bc74a33d55504
Aryan Dadnam <84446295+AryaD1379@users.noreply.github.com>
2025-05-16 00:37:41 -0700

    updating app

--- app.py
+++ app.py
@@ -86,4 +86,5 @@ def balloons():
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # use PORT env var
+    app.run(host="0.0.0.0", port=port, debug=True)
